397704ebaf7b3634f5a6a3375e72251b5e39143c
FatherOfCurses <colin.principe@gmail.com>
2022-07-17 21:09:17 -0400

    Add database connection, morgan, handlebars

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const express = require('express')
 const dotenv = require('dotenv')
+const express = require('express')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const connectDB = require("./config/db")
 
+dotenv.config()
 // load config
-dotenv.config({path: './config/config.env'})
+connectDB().then()
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3000
+// logging
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// Routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(
-    PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
