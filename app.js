cf0a9fd39c11a625e3f79d3111634d09e45aa916
FatherOfCurses <colin.principe@gmail.com>
2022-07-22 13:30:11 -0400

    Fix error where stories were not being displayed on dashboard

--- app.js
+++ app.js
@@ -16,24 +16,13 @@ dotenv.config()
 // Passport config
 require('./config/passport')(passport)
 
-connectDB()
+connectDB().then()
 
 const app = express();
 
 // Body parser
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
-
-app.use(
-    methodOverride(function (req, res) {
-        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-            // look in urlencoded POST bodies and delete it
-            let method = req.body._method
-            delete req.body._method
-            return method
-        }
-    })
-)
 
 // logging
 if(process.env.NODE_ENV === 'development') {
